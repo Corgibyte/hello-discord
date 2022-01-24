eb52f105516f2f790cbc94be744a00e57f1f0a12
Hannah Young <hannah@corgibyte.com>
2022-01-23 18:47:41 -0800

    Implement slow echo

--- HelloDiscord/Modules/HelloModule.cs
+++ HelloDiscord/Modules/HelloModule.cs
@@ -2,13 +2,20 @@ using Discord.Commands;
 
 namespace HelloDiscord.Modules
 {
+  [Group("say")]
   public class HelloModule : ModuleBase<SocketCommandContext>
   {
     //!say hello world -> hello world
-    [Command("say")]
+    [Command("echo")]
     [Summary("Echoes a message.")]
     public Task SayAsync([Remainder][Summary("The text to echo")] string echo) => ReplyAsync(echo);
 
-    // ReplyAsync is a method on ModuleBase
+    [Command("slow", RunMode = RunMode.Async)]
+    [Summary("Slowly echoes a message")]
+    public async Task RepeatAsync([Remainder][Summary("The text to echo")] string echo)
+    {
+      await Task.Delay(TimeSpan.FromSeconds(10));
+      await ReplyAsync(echo);
+    }
   }
 }
